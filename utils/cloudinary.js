ebec873153e473957a21b2f99a4f6f784b47488d
Ochirzul-oss <59369445+Ochirzul-oss@users.noreply.github.com>
2025-04-14 13:05:29 +0800

    aldaa zassan

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
@@ -7,14 +6,4 @@ cloudinary.config({
     api_secret: process.env.cloudinary_Config_api_secret,
 });
 
-=======
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-    cloud_name: process.env.cloudinary_Config_Cloud_Name,
-    api_key: process.env.cloudinary_Config_api_key,
-    api_secret: process.env.cloudinary_Config_api_secret,
-});
-
->>>>>>> 975b7613698d0bae08f4b5d1afe00d19b2d1aec0
 module.exports = cloudinary;
--- utils/emailService.js
+++ utils/emailService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require("http");
 const nodemailer = require('nodemailer');
 
@@ -30,37 +29,4 @@ async function sendEmail(to, subject, text, html) {
   }
 }
 
-=======
-const http = require("http");
-const nodemailer = require('nodemailer');
-
-// Configure the SMTP transporter
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com', // e.g., 'smtp.gmail.com' for Gmail
-  port: 465, // or 465 for secure
-  secure: true, // true for port 465, false for other ports
-  auth: {
-    user: process.env.EMAIL, // your SMTP username
-    pass: process.env.EMAIL_PASS,    // your SMTP password
-  },
-});
-
-// Function to send email
-async function sendEmail(to, subject, text, html) {
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html, // html body
-    });
-    return { success: true, messageId: info.messageId };
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return { success: false, error: error.message };
-  }
-}
-
->>>>>>> 975b7613698d0bae08f4b5d1afe00d19b2d1aec0
 module.exports = { sendEmail };
