4c826e7d99348b1fa7b518d7de799b9ccf095073
Ochirzul-oss <59369445+Ochirzul-oss@users.noreply.github.com>
2025-04-21 15:37:52 +0800

    Update bachelorprice.js

--- models/bachelorprice.js
+++ models/bachelorprice.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const bachelorPriceSchema = mongoose.Schema({
     year: {
-        type: Number,
+        type: String,
         required: true,
     },
     price: {
-        type: Number,
+        type: String,
         required: true,
     },
     dateCreated: {
